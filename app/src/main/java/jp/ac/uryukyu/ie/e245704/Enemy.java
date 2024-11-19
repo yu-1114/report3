9d7d87485446c300ca1d665da785aeaab5ea05f6
Yu Takeshima <e245704@ie.u-ryukyu.ac.jp>
2024-11-19 22:20:50 +0900

    2nd

--- app/src/main/java/jp/ac/uryukyu/ie/e245704/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e245704/Enemy.java
@@ -34,6 +34,9 @@ public class Enemy {
      * @param hero 攻撃対象
      */
     public void attack(Hero hero){
+        if(dead == true){
+            attack = 0;
+        }
         int damage = (int)(Math.random() * attack);
         System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.name, damage);
         hero.wounded(damage);
